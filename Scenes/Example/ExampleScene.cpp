a927bab50398b4a778b7ddc11c5302779c285cc6
Sakirma <huseyincaliskan32@gmail.com>
2020-11-19 12:17:31 +0100

    changed objects

--- Scenes/Example/ExampleScene.cpp
+++ Scenes/Example/ExampleScene.cpp
@@ -8,24 +8,30 @@ void ExampleScene::initialize() {
     EntityId object1 = game->createEntity();
     PhysicsComponent *physicsComponent1 = new PhysicsComponent(object1,
                                                                BodyType::Dynamic,
-                                                               Vector2(350, 50),
+                                                               Vector2(450, 50),
                                                                Vector2(100, 50));
 
     EntityId object2 = game->createEntity();
     PhysicsComponent *physicsComponent2 = new PhysicsComponent(object2,
                                                                BodyType::Static,
-                                                               Vector2(460, 200),
+                                                               Vector2(640, 200),
                                                                Vector2(100, 50));
 
     EntityId object3 = game->createEntity();
     PhysicsComponent *physicsComponent3 = new PhysicsComponent(object3,
                                                                BodyType::Static,
-                                                               Vector2(800, 500),
+                                                               Vector2(1000, 500),
                                                                Vector2(600, 50));
 
+    EntityId object4 = game->createEntity();
+    PhysicsComponent *physicsComponent4 = new PhysicsComponent(object4,
+                                                               BodyType::Static,
+                                                               Vector2(500, 800),
+                                                               Vector2(600, 50));
 
     // TODO: Create Entity class which stores all the components as smart pointers?
     game->addComponent(object1, physicsComponent1);
     game->addComponent(object2, physicsComponent2);
     game->addComponent(object3, physicsComponent3);
+    game->addComponent(object3, physicsComponent4);
 }
