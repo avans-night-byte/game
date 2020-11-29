795c574bf1412c619fbb39f78e2475b269a9e4cd
Yannick Willems <ywillems@protonmail.com>
2020-11-29 13:39:21 +0100

    Play button WIP

--- Scenes/Menu/MainMenu.cpp
+++ Scenes/Menu/MainMenu.cpp
@@ -6,6 +6,20 @@ Spritesheet *characterSpriteSheet;
 Spritesheet *buttonSpriteSheet;
 Spritesheet *settingsSpriteSheet;
 
+// Coords
+int menuButtonWidth = 779;
+int menuButtonHeight = 108;
+int menuButtonX = 100;
+int playButtonY = 26;
+int helpButtonY = 152;
+int creditsButtonY = 278;
+int exitButtonY = 704;
+// Draw settings buttons
+int settingFadeX = 1680;
+int settingsFadeY = 925;
+int settingsBarX = 1760;
+int settingsBarY = 940;
+
 void MainMenu::init(EngineRenderingAPI *engineRenderingAPI, EngineWindowAPI *engineWindowAPI, AudioAPI *audioApi)
 {
   // Load textures
@@ -19,7 +33,7 @@ void MainMenu::init(EngineRenderingAPI *engineRenderingAPI, EngineWindowAPI *eng
 
   // Load sprites
   characterSpriteSheet = engineRenderingAPI->createSpriteSheet("../../Resources/Sprites/character.png", "spritesheet_char", 8, 11, 100, 105);
-  buttonSpriteSheet = engineRenderingAPI->createSpriteSheet("../../Resources/Sprites/buttons.png", "spritesheet_buttons", 2, 3, 914, 226);
+  buttonSpriteSheet = engineRenderingAPI->createSpriteSheet("../../Resources/Sprites/main_menu_buttons.png", "spritesheet_buttons", 2, 3, 914, 226);
   settingsSpriteSheet = engineRenderingAPI->createSpriteSheet("../../Resources/Sprites/settings.png", "spritesheet_settings", 0, 4, 232, 122);
 
   // Init character state
@@ -55,25 +69,41 @@ void MainMenu::render(EngineRenderingAPI *engineRenderingAPI, EngineWindowAPI *e
   // Draw buttons
   engineRenderingAPI->drawTexture("menu_bar", 60, -45, 990, 1170, 1, 1);
 
+  // Play Button
   buttonSpriteSheet->select_sprite(0, 0);
-  buttonSpriteSheet->draw_selected_sprite(100, 26);
+  buttonSpriteSheet->draw_selected_sprite(menuButtonX, playButtonY);
 
   buttonSpriteSheet->select_sprite(0, 1);
-  buttonSpriteSheet->draw_selected_sprite(100, 152);
+  buttonSpriteSheet->draw_selected_sprite(menuButtonX, helpButtonY);
 
   buttonSpriteSheet->select_sprite(0, 2);
-  buttonSpriteSheet->draw_selected_sprite(100, 278);
+  buttonSpriteSheet->draw_selected_sprite(menuButtonX, creditsButtonY);
 
   buttonSpriteSheet->select_sprite(1, 0);
-  buttonSpriteSheet->draw_selected_sprite(100, 704);
+  buttonSpriteSheet->draw_selected_sprite(menuButtonX, exitButtonY);
 
-  // Draw settings buttons
   settingsSpriteSheet->select_sprite(0, 1);
-  settingsSpriteSheet->draw_selected_sprite(1920-240, 1080-155);
+  settingsSpriteSheet->draw_selected_sprite(settingFadeX, settingsFadeY);
   settingsSpriteSheet->select_sprite(0, 3);
-  settingsSpriteSheet->draw_selected_sprite(1920-157, 1080-140);
+  settingsSpriteSheet->draw_selected_sprite(1920 - 157, 1080 - 140);
   settingsSpriteSheet->select_sprite(0, 0);
-  settingsSpriteSheet->draw_selected_sprite(1920-215, 1080-130);
+  settingsSpriteSheet->draw_selected_sprite(1920 - 215, 1080 - 130);
   settingsSpriteSheet->select_sprite(0, 2);
-  settingsSpriteSheet->draw_selected_sprite(1920-145, 1080-147);
+  settingsSpriteSheet->draw_selected_sprite(1920 - 145, 1080 - 147);
+
+  // Click on menu buttons
+  if (i.keyMap.action == "CLICK_LEFT")
+  {
+    clicked(i.x, i.y);
+  }
+}
+
+void MainMenu::clicked(int x, int y)
+{
+  std::cout << "CLICKED!" << std::endl;
+  if (x >= menuButtonX && y >= playButtonY 
+  && x <= (menuButtonX + menuButtonWidth) && y <= (playButtonY + menuButtonHeight))
+  {
+    std::cout << "Clicked PLAY button" << std::endl;
+  }
 }
--- Scenes/Menu/MainMenu.hpp
+++ Scenes/Menu/MainMenu.hpp
@@ -5,4 +5,5 @@ class MainMenu
 public:
   static void init(EngineRenderingAPI *renderAPI, EngineWindowAPI *engineWindowAPI, AudioAPI *audioApi);
   static void render(EngineRenderingAPI *renderAPI, EngineWindowAPI *engineWindowAPI, Input i);
+  static void clicked(int x, int y);
 };
